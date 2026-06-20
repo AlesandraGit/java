c85fca88db2033c1955525734057f9cbc4fd9e18
AlesandraGit <122651019+AlesandraGit@users.noreply.github.com>
2026-06-19 21:37:37 -0300

    instanciandoCategorias
    populando tabela Categoria

--- cursomc/src/main/java/com/alesandra/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/alesandra/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.alesandra.cursomc;
 
+import com.alesandra.cursomc.domain.Categoria;
+import com.alesandra.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
